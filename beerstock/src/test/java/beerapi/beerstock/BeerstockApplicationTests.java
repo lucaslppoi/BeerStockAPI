86431e6caa7e69e6875b06009fb7ae7b0b10bd5c
lucaslppoi <lucaslorenzaop@gmail.com>
2021-07-14 23:18:25 -0300

    POM Plugins

---
+++ beerstock/src/test/java/beerapi/beerstock/BeerstockApplicationTests.java
@@ -0,0 +1,13 @@
+package beerapi.beerstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
